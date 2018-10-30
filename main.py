7abb629aaeb5791365906e40c4619d47c5c19c39
Marcel Santos <mss8@cin.ufpe.br>
2018-10-30 15:26:23 -0300

    Fix the data loader, add momentum in SGD and add csv_logger and reduce_lr callbacks.

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
+import datetime
 import cv2
 import glob
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import time
 
 from PIL import Image
 
-from keras import optimizers
+from keras import optimizers, callbacks
 from keras.utils import np_utils
 from models.SegNet import SegNet
 from initialize import FLAGS
 
-def data_gen(images_dir, labels_dir, nb_classes, batch_size=16, image_size=(320, 320)):
+def data_gen(images_dir, labels_dir, nb_classes=21, batch_size=8, image_size=(320, 320)):
     """
     Generator to yield batches of two inputs (per sample) with shapes top_dim and 
     bot_dim along with their labels.
     """
     images = glob.glob(os.path.join(images_dir, '*.jpg'))
-    labels = glob.glob(os.path.join(labels_dir, '*.png'))
-    assert(len(images) == len(labels))
-
     data_size = len(images)
     i = 0
 
@@ -33,18 +32,22 @@ def data_gen(images_dir, labels_dir, nb_classes, batch_size=16, image_size=(320,
             image = cv2.resize(image, image_size, cv2.INTER_NEAREST)
 
             # Label
-            Y = np.array(Image.open(labels[i]))
+            label_file = os.path.join(labels_dir, 
+                                      os.path.splitext(os.path.basename(images[i]))[0] + '.png')
+
+            Y = np.array(Image.open(label_file))
             Y[Y == 255] = 0
             Y = np_utils.to_categorical(Y, nb_classes)
             Y = cv2.resize(Y, image_size)
             label = Y.reshape(image_size[0] * image_size[1], nb_classes).astype(np.int8)
-
+    
             top_batch.append(image)
             batch_labels.append(label)
 
             i += 1
 
             if i >= data_size:
+                # Shuffle??
                 i = 0
 
         yield np.array(top_batch), np.array(batch_labels)
@@ -52,15 +55,27 @@ def data_gen(images_dir, labels_dir, nb_classes, batch_size=16, image_size=(320,
 def main():
     # Create the model.
     model = SegNet(input_shape=(320,320), classes=FLAGS.numClasses)
-    model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.001), metrics=['acc'])
+    model.summary()
+    # model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.001, momentum=0.9), metrics=['acc'])
 
     # Read the dataset.
     train_generator = data_gen(FLAGS.trainImageDir, FLAGS.trainLabelsDir, FLAGS.numClasses)
     validation_generator = data_gen(FLAGS.valImagesDir, FLAGS.valLabelsDir, FLAGS.numClasses)
 
+    # Create the CSV Logger callback
+    csv_logger = callbacks.CSVLogger('logs/training_%s.log' % (datetime.datetime.now()))
+
+    reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, 
+                                            patience=4, min_lr=0.00003, verbose=1)
+
     # Train the model.
+    print('[INFO] Started training.')
+    start_time = time.time()
     history = model.fit_generator(train_generator, steps_per_epoch=1000, epochs=30,
-                                  validation_data=validation_generator, validation_steps=500)
+                                  validation_data=validation_generator, validation_steps=500,
+                                  callbacks=[csv_logger, reduce_lr])
+
+    print('[INFO] Train took: %s' % (time.time() - start_time))
 
     if FLAGS.saveModel:
         # serialize model to JSON
