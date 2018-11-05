1ae8011ab764e7b94b4b248ffc968a270c0c3a27
Marcel Santana <mss8@cin.ufpe.br>
2018-11-04 22:32:54 -0300

    Fix the saveModel flag and add saveFinalWeights flag.

--- initialize.py
+++ initialize.py
@@ -7,8 +7,14 @@ import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
 
+tf.app.flags.DEFINE_string  ('experimentName', 'seg_net_skip',
+                            'Name of the experiment.')
+
 tf.app.flags.DEFINE_boolean ('saveModel', 1, 
-                            'Indicate whether the network is training or testing.')
+                            'Indicate whether the model json must be saved.')
+
+tf.app.flags.DEFINE_boolean ('saveFinalWeights', 1, 
+                            'Indicate whether the final weights of the model must be saved.')
 
 tf.app.flags.DEFINE_integer ('numClasses', 21,
 							'Indicate the number of classes on the dataset.')
